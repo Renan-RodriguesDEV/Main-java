dd462e2a5dde543ca9bbe8c070882c166bbb01ff
Dev-RodriguesRenan <dev.rodrigues.renan@gmail.com>
2024-04-16 08:10:00 -0300

    metodo fibonacci

---
+++ src/aulasmarcao/src/orientacoaobjetos/matematicos/Fibo.java
@@ -0,0 +1,17 @@
+package matematicos;
+
+public class Fibo {
+    public static void main(String[] args) {
+        System.out.println(fibo(5));
+    }
+
+    static int fibo(int n) {
+        int a = 1, b = 1, c = 0;
+        for (int i = a + b; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
